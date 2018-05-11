69f1c2f54b51170546b7c68beab37218b7fa9743
Erik Hoy <erikshoy@gmail.com>
2018-05-10 19:52:18 -0600

    First commit

---
+++ includes/nav.php
@@ -0,0 +1,14 @@
+<nav id="navbar-example" class="navbar navbar-expand-md navbar-dark fixed-top w3-black">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    	<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarCollapse">
+		<ul class="navbar-nav mr-auto">
+        	<li class="nav-item"><a class="nav-link" href="#main">Home</a></li>
+          	<li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+          	<li class="nav-item"><a class="nav-link" href="#skills">Skills</a></li>
+          	<li class="nav-item"><a class="nav-link" href="#gallery">Gallery</a></li>
+          	<li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+        </ul>
+    </div>
+</nav>
